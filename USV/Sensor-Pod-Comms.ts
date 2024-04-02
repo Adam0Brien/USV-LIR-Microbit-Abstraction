ca10e663899ca41876c94fa6eecfb41502a8fcc0
Adam0Brien <20093460@mail.wit.ie>
2024-04-02 15:23:18 +0100

    feat: Added Sensor Pod to USV Comms.

---
+++ USV/Sensor-Pod-Comms.ts
@@ -0,0 +1,59 @@
+// Starter Implementation
+// Change on button A and B pressed to physical circuit
+input.onButtonPressed(Button.A, function () {
+    isSubmerged = 0
+})
+input.onButtonPressed(Button.B, function () {
+    isSubmerged = 1
+})
+
+
+let isSubmerged = 0
+radio.setGroup(213)
+basic.showIcon(IconNames.Yes)
+USVSensorPod.initLocalDisplay()
+let ph_list: number[] = []
+let light_list: number[] = []
+let temp_list: number[] = []
+isSubmerged = 1
+basic.forever(function () {
+    USVSensorPod.ShowString("Temp:" + USVSensorPod.calculateTempC() + "°C", 0, 0)
+    USVSensorPod.ShowString("pH:" + USVSensorPod.calculatePh(), 0, 1)
+})
+basic.forever(function () {
+    if (isSubmerged == 1) {
+        ph_list.push(USVSensorPod.calculatePh())
+        temp_list.push(USVSensorPod.calculateTempC())
+        light_list.push(USVSensorPod.getLight())
+        basic.pause(100)
+        led.toggle(4, 0)
+    }
+})
+basic.forever(function () {
+    if (isSubmerged == 0) {
+        for (let value of ph_list) {
+            radio.sendValue("pH", value)
+            basic.pause(1000)
+            ph_list.removeAt(ph_list.indexOf(value))
+        }
+    }
+})
+basic.forever(function () {
+    if (isSubmerged == 0) {
+        for (let value of light_list) {
+            radio.sendValue("l", value)
+            basic.pause(1000)
+            led.toggle(0, 0)
+            light_list.removeAt(light_list.indexOf(value))
+        }
+    }
+})
+basic.forever(function () {
+    if (isSubmerged == 0) {
+        for (let value of temp_list) {
+            radio.sendValue("tempC", value)
+            basic.pause(100)
+            temp_list.removeAt(temp_list.indexOf(value))
+        }
+    }
+})
