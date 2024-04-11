cf591f4be2af03e4e8b141940bc5be89cf3272e5
Adam0Brien <20093460@mail.wit.ie>
2024-04-11 14:30:46 +0100

    USV: Added Manual motor control block + Added default time values

--- USV/USV.ts
+++ USV/USV.ts
@@ -54,7 +54,8 @@ namespace USV {
     //% subcategory=USV
     //% group="B. Movement"
     //% blockId=USV_Motor block="Move |%Motor|%speed for |%ms seconds"
-    //% speed.min=60 speed.max=120
+    //% speed.min=60 speed.max=120 speed.defl=90
+    //% sec.min=1 sec.max=120 sec.defl=1
     //% index.fieldEditor="gridpicker" index.fieldOptions.columns=2
     export function USV_Motor(motor: Motors, speed: Speed, sec: number): void {
         if(motor == 0) {
@@ -90,6 +91,50 @@ namespace USV {
         }
     }
 
+    /**
+        * Configure the USV Servos manually.
+        */
+    //% weight=50 
+    //% color=#7F74D5
+    //% subcategory=USV
+    //% group="B. Movement"
+    //% blockId=USV_Motor_Control block="Move |%Motor|%speed for |%ms seconds"
+    //% speed.min=60 speed.max=120 speed.defl=90
+    //% sec.min=1 sec.max=120 sec.defl=1
+    //% index.fieldEditor="gridpicker" index.fieldOptions.columns=2
+    export function USV_Motor_Contol(motor: Motors, speed: number, sec: number): void {
+        if (motor == 0) {
+            if (Arm == 1) {
+                for (let index = 0; index <= sec; index++) {
+                    for (let index = 0; index <= 10; index++) {
+                        basic.pause(100)
+                        radio.sendValue("left", 180 - speed)
+                    }
+                }
+            }
+        }
+        if (motor == 1) {
+            if (Arm == 1) {
+                for (let index = 0; index <= sec; index++) {
+                    for (let index = 0; index <= 10; index++) {
+                        basic.pause(100)
+                        radio.sendValue("right", speed)
+                    }
+                }
+            }
+        }
+        if (motor == 2) {
+            if (Arm == 1) {
+                for (let index = 0; index <= sec; index++) {
+                    for (let index = 0; index <= 10; index++) {
+                        radio.sendValue("left", 180 - speed)
+                        radio.sendValue("right", speed)
+                        basic.pause(100)
+                    }
+                }
+            }
+        }
+    }
     //% blockId=stopMotors
     //% block="Stop Motors"
     //% weight=50 
@@ -108,7 +153,6 @@ namespace USV {
         //%blockId=sensor_up
         //% block="Up"
         up = 1
-        
     }
 
     /**
@@ -120,16 +164,17 @@ namespace USV {
     //% group="C. Sensor Deployment"
     //% blockId=USV_Deploy
     //% block="Sensor Deploy |%Motor for %number seconds"
+    //% sec.min=1 sec.max=120 sec.defl=1
     //% index.fieldEditor="gridpicker" index.fieldOptions.columns=2
-    export function deployDirection(sensorState: SensorDeploy, value: number): void {
+    export function deployDirection(sensorState: SensorDeploy, sec: number): void {
         if (sensorState == 0) {
             radio.sendValue("down", 0)
-            basic.pause(value * 1000)
+            basic.pause(sec * 1000)
             radio.sendValue("stop", 0)
         }
         if (sensorState == 1) {
             radio.sendValue("up", 0)
-            basic.pause(value * 1000)
+            basic.pause(sec * 1000)
             radio.sendValue("stop", 0)
         }
     }
@@ -165,6 +210,7 @@ namespace USV {
     //% group="D. Auto Movement"
     //% blockId=AUTO_USV block="Auto Drive |%Direction for %number seconds"
     //% speed.min=60 speed.max=120
+    //% sec.min=1 sec.max=120 sec.defl=1
     //% index.fieldEditor="gridpicker" index.fieldOptions.columns=2
     export function AUTO_USV(direction: Direction, sec: number): void {
         for (let index = 0; index <= sec; index++) {
